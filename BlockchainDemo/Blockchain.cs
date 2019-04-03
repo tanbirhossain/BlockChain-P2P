3a6681c53737556ff5086312385a6e9386303c62
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-04-03 15:55:56 +0900

    Add project files.

---
+++ BlockchainDemo/Blockchain.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockchainDemo
+{
+    public class Blockchain
+    {
+        public IList<Transaction> PendingTransactions = new List<Transaction>();
+        public IList<Block> Chain { set;  get; }
+        public int Difficulty { set; get; } = 2;
+        public int Reward = 1; //1 cryptocurrency
+
+        public Blockchain()
+        {
+            
+        }
+
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+            AddGenesisBlock();
+        }
+
+        public Block CreateGenesisBlock()
+        {
+            Block block = new Block(DateTime.Now, null, PendingTransactions);
+
+            block.Mine(Difficulty);
+            PendingTransactions = new List<Transaction>();
+            return block;
+        }
+
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+        
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+        /// <summary>
+        /// 1. CreateTransaction . simple add a transaction into transaction list
+        /// </summary>
+        /// <param name="transaction"></param>
+        public void CreateTransaction(Transaction transaction)
+        {
+            PendingTransactions.Add(transaction); 
+        }
+        /// <summary>
+        /// 2. 2nd minar part. this time transaction will finalized
+        /// </summary>
+        /// <param name="minerAddress"></param>
+        public void ProcessPendingTransactions(string minerAddress)
+        {
+            //we have to keep in mind every time mining means  one block complete .
+            //all pending transaction and previous transaction hash linked in this block
+            // after block complere all previous transaction list will be empty
+
+            Block block = new Block(DateTime.Now, GetLatestBlock().Hash, PendingTransactions);//B-1
+
+            //here is populate block other variables and this block to the Block list chain
+            AddBlock(block);
+
+            // already we have created block so now we will create new tranactionlist, 
+            // now pending transationlist fully empty
+            PendingTransactions = new List<Transaction>(); 
+            CreateTransaction(new Transaction(null, minerAddress, Reward));
+        }
+
+        public void AddBlock(Block block) //part of - ProcessPendingTransactions
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;//3
+            block.PreviousHash = latestBlock.Hash;//4 -- this is no need to push here bcoz we already populate this hash
+            //block.Hash = block.CalculateHash();
+            block.Mine(this.Difficulty);//5 - this takes so many much time for making the  Difficulty label hash
+
+
+           //whole block chain here. this chain list store all completed trasaction block
+            Chain.Add(block);
+        }
+
+        public bool IsValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int GetBalance(string address)
+        {
+            int balance = 0;
+
+            for (int i = 0; i < Chain.Count; i++)
+            {
+                for (int j = 0; j < Chain[i].Transactions.Count; j++)
+                {
+                    var transaction = Chain[i].Transactions[j];
+
+                    if (transaction.FromAddress == address)
+                    {
+                        balance -= transaction.Amount;
+                    }
+
+                    if (transaction.ToAddress == address)
+                    {
+                        balance += transaction.Amount;
+                    }
+                }
+            }
+
+            return balance;
+        }
+    }
+}
---
+++ BlockchainDemo/P2PClient.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebSocketSharp;
+
+namespace BlockchainDemo
+{
+    public class P2PClient
+    {
+        IDictionary<string, WebSocket> wsDict = new Dictionary<string, WebSocket>();
+
+        /// <summary>
+        /// 1. client trying to connect to server . 
+        /// 2. ws.Connect(); -- call and connect with server
+        /// 3. ws.Send("Hi Server");　 -- send message to server
+        /// 4. P2PServer.Send("Hi Client"); -- after getting  message from client -> server send message to client Send("Hi Client")
+        /// 
+        /// 5.client again send message to -> server with  all transaction and  server also send client to all transaction >>>
+        /// *** so basically when two client connected each other firstly they share there all transactions.
+        ///     when they recive others transactions they at first they validate the whole transactions
+        ///     after that they synched the this thransactrio
+        ///     
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        public void Connect(string url)
+        {
+            if (!wsDict.ContainsKey(url))
+            {
+                WebSocket ws = new WebSocket(url);
+                ws.OnMessage += (sender, e) =>
+                {
+                    if (e.Data == "Hi Client")
+                    {
+                        Console.WriteLine(e.Data);
+                    }
+                    else
+                    {
+                        Blockchain newChain = JsonConvert.DeserializeObject<Blockchain>(e.Data);
+                        if (newChain.IsValid() && newChain.Chain.Count > Program.PhillyCoin.Chain.Count)
+                        {
+                            List<Transaction> newTransactions = new List<Transaction>();
+                            newTransactions.AddRange(newChain.PendingTransactions);
+                            newTransactions.AddRange(Program.PhillyCoin.PendingTransactions);
+
+                            newChain.PendingTransactions = newTransactions;
+                            Program.PhillyCoin = newChain;
+                        }
+                    }
+                };
+                
+                ws.Connect();
+                ws.Send("Hi Server");　
+                ws.Send(JsonConvert.SerializeObject(Program.PhillyCoin));
+                wsDict.Add(url, ws);
+            }
+        }
+
+        public void Send(string url, string data)
+        {
+            foreach (var item in wsDict)
+            {
+                if (item.Key == url)
+                {
+                    item.Value.Send(data);
+                }
+            }
+        }
+        
+        public void Broadcast(string data)
+        {
+            foreach (var item in wsDict)
+            {
+                item.Value.Send(data);
+            }
+        }
+
+        public IList<string> GetServers()
+        {
+            IList<string> servers = new List<string>();
+            foreach (var item in wsDict)
+            {
+                servers.Add(item.Key);
+            }
+            return servers;
+        }
+
+        public void Close()
+        {
+            foreach (var item in wsDict)
+            {
+                item.Value.Close();
+            }
+        }
+    }
+}
